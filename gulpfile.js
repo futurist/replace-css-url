0ada7fb5ca26ea72e446b4105be07a07e99766c1
1111hui <26634873@qq.com>
2016-10-27 17:29:06 +0800

    build: add gulp tag task for publish current version

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-
+var fs = require('fs')
+var exec = require('child_process').exec
 var gulp = require('gulp')
 var bump = require('gulp-bump')
 
@@ -10,3 +11,7 @@ gulp.task('bump', function () {
     .pipe(gulp.dest('./'))
 })
 
+gulp.task('tag', function () {
+  var version = JSON.parse(fs.readFileSync('./package.json', 'utf8')).version
+  exec('git tag ' + version + ' && git push --tags', err => err)
+})
