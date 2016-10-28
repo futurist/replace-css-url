d4e7000dde716c29e33868578ecef828ad548fb6
1111hui <26634873@qq.com>
2016-10-28 18:49:44 +0800

    build: fix source map path

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 
 import {writeFileSync} from 'fs'
 import buble from 'rollup-plugin-buble'
-import uglify from 'uglify-js'
+import minify from 'rollup-plugin-minify'
 
 export default {
   entry: './src/replace-css-url.js',
@@ -17,24 +17,4 @@ export default {
     { format: 'amd',  dest: 'dist/replace-css-url.amd.js'  },
     { format: 'iife', dest: 'dist/replace-css-url.iife.js' },
   ]
-}
-
-function minify (minifyMap) {
-  return {
-    name: 'minify',
-    transformBundle (code, option) {
-      Object.keys(minifyMap).forEach(format => {
-        if (option.format == format) {
-          const name = minifyMap[format]
-          const mapName = name + '.map'
-          const result = uglify.minify( code, {
-            fromString: true,
-            outSourceMap: mapName
-          })
-          writeFileSync(name, result.code, 'utf8')
-          writeFileSync(mapName, result.map, 'utf8')
-        }
-      })
-    }
-  }
 }
